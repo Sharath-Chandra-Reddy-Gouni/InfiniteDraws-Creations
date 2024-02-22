2b139798b1e2ce8995e71f3150d16688310d73f1
Parvinder Singh <W0845628@myscc.ca>
2024-02-22 12:57:29 -0500

    Changes in SeedData

--- Models/Drawer.cs
+++ Models/Drawer.cs
@@ -15,17 +15,16 @@ namespace InfiniteDraws_Creations.Models
         [StringLength(60, MinimumLength = 3)]
         [Required]
         public string? Color { get; set; }
-        
+
         [Range(1, 100)]
         [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+
+        public Decimal Rating { get; set; }
         [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
-        [StringLength(5)]
-        [Required]
-        public string? Rating { get; set; }
-        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
-        [StringLength(5)]
+        [StringLength(30)]
         [Required]
         public string? Reviews { get; set; }
 
